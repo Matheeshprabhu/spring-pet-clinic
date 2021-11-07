191ef87745382f33e795d8aa6d99168304688865
Matheesh <matheeshprabhu123@gmail.com>
2021-11-07 15:56:27 +0530

    Adding index and controller to list owners

---
+++ pet-clinic-web/src/main/java/com/math/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.math.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "index"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+}
