00d05c8e60566f6beb2d56057de784559615ccbc
Matheesh <matheeshprabhu123@gmail.com>
2021-11-11 17:45:43 +0530

    Displaying List of owners

--- pet-clinic-web/src/main/java/com/math/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/math/petclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package com.math.petclinic.controllers;
 
+import com.math.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired // Autowired is not required when we use constructor injection
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "index"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
