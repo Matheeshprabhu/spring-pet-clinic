bfa9630a0121585a37adf2d21b9add6cbd15918c
Matheesh <matheeshprabhu123@gmail.com>
2021-11-11 18:00:41 +0530

    Displaying List of vets

--- pet-clinic-web/src/main/java/com/math/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/math/petclinic/controllers/VetController.java
@@ -1,15 +1,24 @@
 package com.math.petclinic.controllers;
 
+import com.math.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
